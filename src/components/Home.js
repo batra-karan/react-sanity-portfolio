1e67ebbe75e8df72d9457e3ae66db788f2621e92
Karan Batra <mrbatrakaran@gmail.com>
2021-06-24 21:17:43 -0400

    finish navbar and setup post blog page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,13 @@
 import React from 'react'
+import bgimage from '../bg-image.jpg'
 
 export default function Home() {
-    return <h2>Home Page</h2>
+    return (
+        <main>
+            <img src={bgimage} alt="Tech" className="absolute object-cover w-full h-full" />
+            <section className="relative flex justify-center min-h-screen pt-12 lg:pt-64 px-8">
+                <h1 className="text-6xl text-green-100 font-bold cursive leading-none lg:leading-snug home-name">Hello, I'm Karan</h1>
+            </section>
+        </main>    
+    )
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,5 +1,30 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
+import { SocialIcon } from 'react-social-icons';
 
 export default function NavBar() {
-    return <h2>NavBar Page</h2>
+    return (
+        <header className="bg-red-900">
+            <div className="container mx-auto flex justify-between">
+                <nav className="flex">
+                    <NavLink className="inline-flex item-center py-6 px-3 mr-4 text-red-100 hover:text-green-800 font-bold text-4xl cursive tracking-widest" to="/" exact activeClassName="text-white">
+                        Karan Batra
+                    </NavLink>
+                    <NavLink className="inline-flex item-center py-3 px-3 my-6 rounded text-red-200 hover:text-green-800" to="/about" activeClassName="text-red-100 bg-red-700">
+                        About
+                    </NavLink>
+                    <NavLink className="inline-flex item-center py-3 px-3 my-6 rounded text-red-200 hover:text-green-800" to="/project" activeClassName="text-red-100 bg-red-700">
+                        Projects
+                    </NavLink>
+                    <NavLink className="inline-flex item-center py-3 px-3 my-6 rounded text-red-200 hover:text-green-800" to="/post" activeClassName="text-red-100 bg-red-700">
+                        Blog Post
+                    </NavLink>
+                </nav>
+                <div className="inline-flex py-3 px-3 my-6">
+                    <SocialIcon url="https://www.linkedin.com/in/mrbatrakaran/" className="mr-4" target="_blank" fgColor="#fff" style={{ height: 35, width: 35}} />
+                    <SocialIcon url="https://github.com/batra-karan" className="mr-4" target="_blank" fgColor="#fff" style={{ height: 35, width: 35}}/>
+                </div>
+            </div>
+        </header>
+    )
 }
